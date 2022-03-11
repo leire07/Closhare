167a17c02a7a75d04cc6b705c7ec35c67215281c
Leire <lvilmar1@epsg.upv.es>
2022-03-11 23:39:01 +0100

    Nav bar no mueras

--- app/src/main/java/com/example/closhare/MainActivity.java
+++ app/src/main/java/com/example/closhare/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.closhare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.chipNavigation);
+
+        chipNavigationBar.setItemSelected(R.id.collection, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CollectionFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.collection:
+                        fragment = new CollectionFragment();
+                        break;
+                    case R.id.create:
+                        fragment = new CreateFragment();
+                        break;
+                    case R.id.gallery:
+                        fragment = new GalleryFragment();
+                        break;
+
+                }
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
     }
 }
